9ec88d4f8b000ad869dd8ffb856912176ca20d78
Vince W <vincewallgren@me.com>
2020-11-10 04:13:25 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,11 +9,12 @@ def won?
   top_row_win = [0,1,2]
   if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
     puts "X won in the top row"
-end
-
-def won?
   board = [" ", " ", " ", "X", "X", "X", " ", " ", " "]
   middle_row_win = [3,4,5]
-  if board[middle_row_win[0]] == "X" && board[middle_row_win[1]] == "X" && board[middle_row_win[2]] == "X"
+  if board[middle_row_win[3]] == "X" && board[middle_row_win[4]] == "X" && board[middle_row_win[5]] == "X"
     puts "X won the middle row"
-end
+  board = [" ", " ", " ", " ", " ", " ", "X", "X", "X"]
+  bottom_row_win = [6,7,8]
+  if board[bottom_row_win[6]] == "X" && board[bottom_row_win[7]] == "X" && board[bottom_row_win[8]] == "X"
+    puts "X won the bottom row"
+  end
