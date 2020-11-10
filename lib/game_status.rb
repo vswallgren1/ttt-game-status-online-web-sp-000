eb589d7d97a3105e8c3641ac80b7231a45941ba1
Vince W <vincewallgren@me.com>
2020-11-10 04:06:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,4 +5,13 @@ end
 
 # Define your WIN_COMBINATIONS constant
 def won?
-  
+  board = ["X", "X", "X,", " ", " ", " ", " ", " ", " "]
+  top_row_win = [0,1,2]
+  if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+  "X won in the top row"
+
+  board = [" ", " ", " ", "X", "X", "X", " ", " ", " "]
+  middle_row_win = [3,4,5]
+  if board[middle_row_win[0]] == "X" && board[middle_row_win[1]] == "X" && board[middle_row_win[2]] == "X"
+    "X won the middle row"
+end
