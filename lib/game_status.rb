0b4b44d7b74dd25e935ac398f4e78fb9c9362187
Vince W <vincewallgren@me.com>
2020-11-10 05:42:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,18 +1,12 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
-
-# Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], #top row
-  [3,4,5], #middle row
-  [6,7,8], #bottom Row
-  [0,3,6], #left column
-  [1,4,7], #middle column
-  [2,5,8], #right column
-  [0,4,8], #left to right diagonal
-  [2,4,6] #right to left diagonal
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ]
 
 def won?(board)
@@ -21,9 +15,9 @@ def won?(board)
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
 
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
       return win_combination
@@ -33,7 +27,7 @@ def won?(board)
 end
 
 def full?(board)
-  if board.any? {|index| index == " " || index == nil}
+  if board.any?{|index| index == " " || index == nil}
     return false
   else
     return true
@@ -45,19 +39,21 @@ def draw?(board)
     return true
   elsif !won?(board) && !full?(board)
     return false
-  else !won(board)
+  else won?(board)
     return false
   end
 end
 
 def over?(board)
-  if won?(board) || full?(board) || draw?(board)
-    return ture
+  if won?(board) || draw?(board) || full?(board)
+    return true
+  else
+    return false
   end
 end
 
 def winner(board)
   if won?(board)
-    return board[won?(board) [0]]
+    return board[won?(board)[0]]
   end
-end
+end 
